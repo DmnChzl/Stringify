ec5233108e97c04b6cdd17477fe9afae219d1dfe
Damien Chazoule <damien.chazoule@gmail.com>
2024-05-28 22:06:20 +0200

    Feat: More Utilities (getWords, countWords)

--- README.md
+++ README.md
@@ -1,12 +1,116 @@
 # Stringify
 
-> 0.1.0
+> 0.2.0
 
-Utilities for string transformation
+Utilities for string transformation, published on [JSR](https://jsr.io).
+
+## Installation
+
+First of all, you need to install the package:
+
+```
+npx jsr add @dmnchzl/stringify
+```
+
+<details>
+  <summary>Equivalent with Yarn</summary>
+  
+  ```
+  yarn dlx jsr add @dmnchzl/stringify
+  ```
+
+</details>
+
+<details>
+  <summary>Equivalent with pNPM</summary>
+  
+  ```
+  pnpm dlx jsr add @dmnchzl/stringify
+  ```
+
+</details>
+
+<details>
+  <summary>Equivalent with Deno</summary>
+  
+  ```
+  deno add @dmnchzl/stringify
+  ```
+
+</details>
+
+<details>
+  <summary>Equivalent with Bun</summary>
+  
+  ```
+  bunx jsr add @dmnchzl/stringify
+  ```
+
+</details>
 
 ## Utilities
 
-Lorem ipsum dolor sit amet...
+Below, the list of all available utilities:
+
+**toCamelCase**
+
+> Set case of a string to 'camelCase'
+
+```js
+import { toCamelCase } from '@dmnchzl/stringify';
+
+console.log(toCamelCase('Hello World')); // helloWorld
+```
+
+**toKebabCase**
+
+> Set case of a string to 'kebab-case'
+
+```js
+import { toKebabCase } from '@dmnchzl/stringify';
+
+console.log(toKebabCase('Hello World')); // hello-world
+```
+
+**toPascalCase**
+
+> Set case of a string to 'PascalCase'
+
+```js
+import { toPascalCase } from '@dmnchzl/stringify';
+
+console.log(toPascalCase('Hello World')); // HelloWorld
+```
+
+**toSnakeCase**
+
+> Set case of a string to 'snake_case'
+
+```js
+import { toSnakeCase } from '@dmnchzl/stringify';
+
+console.log(toSnakeCase('Hello World')); // hello_world
+```
+
+**getWords**
+
+> Extract words from string
+
+```js
+import { getWords } from '@dmnchzl/stringify';
+
+console.log(getWords('Hello World')); // ['Hello', 'World']
+```
+
+**countWords**
+
+> Count words from string
+
+```js
+import { countWords } from '@dmnchzl/stringify';
+
+console.log(countWords('Hello World')); // 2
+```
 
 ## License
 
--- index.ts
+++ index.ts
@@ -8,6 +8,12 @@
  */
 const REGEX = /(?:^\w|[A-Z]|\b\w|\s+|[-_])/g;
 
+/**
+ * Set case of a string to 'camelCase'
+ *
+ * @param {string} str
+ * @returns {string} camelCase
+ */
 export function toCamelCase(str: string): string {
   return str.toLowerCase().replace(REGEX, (match, idx) => {
     if (+match === 0 || match === '-' || match === '_') return ''; // Remove dashes and underscores
@@ -15,6 +21,12 @@ export function toCamelCase(str: string): string {
   });
 }
 
+/**
+ * Set case of a string to 'kebab-case'
+ *
+ * @param {string} str
+ * @returns {string} kebab-case
+ */
 export function toKebabCase(str: string): string {
   return str
     .toLowerCase()
@@ -23,6 +35,12 @@ export function toKebabCase(str: string): string {
     .replace(/[-]+/g, '-'); // Remove multiple consecutive dashes
 }
 
+/**
+ * Set case of a string to 'PascalCase'
+ *
+ * @param {string} str
+ * @returns {string} PascalCase
+ */
 export function toPascalCase(str: string): string {
   return str.toLowerCase().replace(REGEX, match => {
     if (match === '-' || match === '_') return ''; // Remove dashes and underscores
@@ -30,10 +48,47 @@ export function toPascalCase(str: string): string {
   });
 }
 
+/**
+ * Set case of a string to 'snake_case'
+ *
+ * @param {string} str
+ * @returns {string} snake_case
+ */
 export function toSnakeCase(str: string): string {
   return str
     .toLowerCase()
     .replace(/[\s-]+/g, '_') // Replace spaces and dashes with underscores
     .replace(/([a-z])([A-Z])/g, '$1_$2') // Add underscore between words
     .replace(/[_]+/g, '_'); // Remove multiple consecutive underscores
+}
+
+/**
+ * Extract words from string
+ *
+ * @param {string} str
+ * @returns {string[]} words
+ */
+export function getWords(str: string): string[] {
+  // Trim the string to remove any leading or trailing whitespace
+  str = str.trim();
+
+  // If the string is empty, return an empty array
+  if (str === '') return [];
+
+  /**
+   * Split the string by non-word characters (excluding apostrophes),
+   * and filter out empty strings...
+   */
+  return str.split(/[^a-zA-Z0-9\u00C0-\u017F']+/).filter(Boolean);
+}
+
+/**
+ * Count words from string
+ *
+ * @param {string} str
+ * @returns {number} Number of words
+ */
+export function countWords(str: string): number {
+  const words = getWords(str);
+  return words.length;
 }
